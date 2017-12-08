4f87cbd7f70e541b5a082cd93ecf3e805a657996
Matthias PAce <telu1999@gmail.com>
2017-12-08 10:25:41 +0100

    Paddles Working

---
+++ ComputerGamesAssignment/New Unity Project/Assets/Scripts/KeyBoardPaddle.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeyBoardPaddle : MonoBehaviour {
+
+    float paddlePos;
+    // Use this for initialization
+    void Start () {
+        paddlePos = transform.position.y;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // float mousePos;
+
+        
+        if (Input.GetKey(KeyCode.W))
+        {
+            if (transform.position.y <= 3.5f)
+            {
+                transform.Translate(Vector3.left * 15f * Time.deltaTime);
+            }
+        }
+        else if (Input.GetKey(KeyCode.S))
+        {
+            if (transform.position.y >= -3.5f)
+            {
+                transform.Translate(-Vector3.left * 15f * Time.deltaTime);
+            }
+        }
+    }
+}
---
+++ ComputerGamesAssignment/New Unity Project/Assets/Scripts/MousePaddle.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MousePaddle : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float mousePosInUnits = (Input.mousePosition.y / Screen.height * 13f) - 6.5f;
+        float mousePos = Mathf.Clamp(mousePosInUnits, -3.5f, 3.5f);
+        Vector3 paddlePosition = new Vector3(this.transform.position.x, mousePos, 0);
+        this.transform.position = paddlePosition;
+    }
+}
