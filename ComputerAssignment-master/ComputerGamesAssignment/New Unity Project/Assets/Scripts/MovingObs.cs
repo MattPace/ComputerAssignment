3de24f0bb694934941e192acfc6b38c9776b8ebb
MattPace <32700189+MattPace@users.noreply.github.com>
2017-12-18 00:38:16 +0100

    Level 3 Working

---
+++ ComputerAssignment-master/ComputerGamesAssignment/New Unity Project/Assets/Scripts/MovingObs.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingObs : MonoBehaviour {
+
+	private Vector3 startPos;
+	private Vector3 secondPos;
+	private Vector3 nextPos;
+
+	[SerializeField]
+	private float speed;
+
+	[SerializeField]
+	private Transform positionTransform2;
+
+	[SerializeField]
+	private Transform transformB;
+
+	void Start () {
+		startPos = positionTransform2.localPosition;
+		secondPos = transformB.localPosition;
+		nextPos = secondPos;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		ObsMove ();
+	}
+
+	private void ObsMove(){
+		positionTransform2.localPosition = Vector3.MoveTowards (positionTransform2.localPosition, nextPos, speed * Time.deltaTime);
+		if (Vector3.Distance (positionTransform2.localPosition, nextPos) <= 0.1) {
+			goBack ();
+		}
+	}
+
+	private void goBack(){
+		nextPos = nextPos != startPos ? startPos : secondPos;
+	}
+}
